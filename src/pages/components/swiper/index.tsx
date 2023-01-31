e1a09771f3bf0ef1c55fd60d8f5f1fb56dedff33
Matthew Araujo <matthewaraujo20@gmail.com>
2023-01-31 15:30:51 -0300

    ainda fazendo o swiper

--- src/pages/components/swiper/index.tsx
+++ src/pages/components/swiper/index.tsx
@@ -1,59 +1,81 @@
-// import Swiper core and required modules
-import { Navigation, Thumbs } from 'swiper'
-
+import React, { useState } from 'react'
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
-
 // Import Swiper styles
 import 'swiper/css'
+import 'swiper/css/free-mode'
 import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
 import 'swiper/css/thumbs'
 
 import styles from './styles.module.scss'
-import { Bicycle } from 'phosphor-react'
+// import required modules
+import { FreeMode, Navigation, Thumbs } from 'swiper'
+import { Bicycle, SoccerBall } from 'phosphor-react'
 import Image from 'next/image'
 
 export default function SwiperLazer() {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null)
+
   return (
-    <div className={styles.container}>
+    <div className={styles.containerSwiper}>
       <Swiper
-        modules={[Navigation, Thumbs]}
-        spaceBetween={10}
-        slidesPerView={1}
-        navigation
-        onSwiper={(swiper) => console.log(swiper)}
-        className={styles.swiper}
+        loop={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper2"
       >
         <SwiperSlide className={styles.swiperSlide}>
-          <Image src="/bike.png" width={645} alt="churrasco" height={496} />
+          <Image src="/bike.png" width={456} alt="churrasco" height={358} />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}>
           <Image src="/barbecue.png" width={456} alt="churrasco" height={358} />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}>
           <Image src="/futebol.png" width={456} alt="churrasco" height={358} />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}>
           <Image src="/barbecue.png" width={456} alt="churrasco" height={358} />
         </SwiperSlide>
       </Swiper>
       <Swiper
+        onSwiper={setThumbsSwiper}
         loop={true}
         spaceBetween={10}
         slidesPerView={1}
-        className={styles.swiperThumbs}
-        onSwiper={(swiper) => console.log(swiper)}
-        navigation
+        modules={[Navigation, Thumbs]}
+        className="mySwiper"
+        navigation={true}
       >
-        <SwiperSlide>
+        <SwiperSlide className={styles.contentThumbs}>
           <div></div>
           <div className={styles.ThumbsText}>
             <div>
               <Bicycle size={32} />
             </div>
             <div>
-              <h1>bicicleta</h1>
+              <h1>Bicicleta</h1>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className={styles.contentThumbs}>
+          <div></div>
+          <div className={styles.ThumbsText}>
+            <div>
+              <SoccerBall size={32} />
+            </div>
+            <div>
+              <h1>Futebol</h1>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className={styles.contentThumbs}>
+          <div></div>
+          <div className={styles.ThumbsText}>
+            <div>
+              <SoccerBall size={32} />
+            </div>
+            <div>
+              <h1>Futebol</h1>
             </div>
           </div>
         </SwiperSlide>
