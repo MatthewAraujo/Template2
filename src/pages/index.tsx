2a4f0b32d2132f3c8a52d77f31dc894dd344a93e
Matthew Araujo <matthewaraujo20@gmail.com>
2023-02-08 16:28:55 -0300

    parte do formulario feito, so falta fazer o footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Banner from './components/Banner'
 import Contact from './components/Contatos_mobile'
 import Cookies from './components/Cookies'
 import Faixada from './components/Faixada'
+import Form from './components/Form'
 import Header from './components/Header'
 import HoverIcons from './components/HoverIcons'
 import Lazer from './components/Lazer'
@@ -13,13 +14,14 @@ export default function Home() {
     <div>
       <Header />
       <Cookies />
-      <Contact />
+      {/* <Contact /> */}
       <HoverIcons />
       <Banner />
       <Faixada />
       <Video />
       <Lazer />
       <Plantas />
+      <Form />
     </div>
   )
 }
